aa5e1e57c1dadd555c4ed8617a3ac2c15fa690d7
Elisabeth Heuston <heustonef@nih.gov>
2026-04-16 10:49:15 -0400

    Typos in function call

--- doublet_detector.R
+++ doublet_detector.R
@@ -5,7 +5,7 @@ doublet_detector <- function(
 		detector, 
 		logger,
 		sctransform = FALSE,
-		sourceable.functions = sourceable.functions,
+		sourceable.functions = "RFunctions",
 		pANN.reuse = NULL,
 		doublet.var.thresh = 90, 
 		predicted.doubletRate = 0.05)
@@ -14,7 +14,7 @@ doublet_detector <- function(
 		msg <- "Running doubletfinder"
 		pring(msg)
 		log.msg(logger, msg = msg)
-		source(sourceable.functions[grepl("runDoubletFinder.R", sourceable.functions)])
+		source(file.path(sourceable.functions, "runDoubletFinder.R"))
 		seurat.object <- runDoubletFinder(seurat.object, 
 																			sctransformed = do.sctransform, 
 																			predicted.doubletRate = predicted.doubletRate,
@@ -24,7 +24,7 @@ doublet_detector <- function(
 		msg <- "Running scrublet"
 		print(msg)
 		log.msg(logger, msg = msg)
-		source(sourceable.functions[grepl("runScrublet.R", sourceable.functions)])
+		source(file.path(sourceable.functions, "runScrublet.R"))
 		seurat.object <- runScrublet(seurat.object)
 		return(seurat.object)
 	} else {
