c3efa6718f318ca672d4b8c73d61780fd95e1bf1
Elisabeth Heuston <heustonef@nih.gov>
2026-04-22 10:13:53 -0400

    Added mouse adip markers from Emont et al., 2022

---
+++ adip.markers.R
@@ -0,0 +1,22 @@
+# Adipose tissue marker genes from emont et al., Nature 2022
+
+adip.markers <- c(
+"Adipoq", #adipocyte
+"Pdgfra", #APSSPC
+"Msln", #mesothelium
+"Jam2", #endothelium
+"Prox1", #lymphatic endo
+"Steap4", #pericyte
+"Myocd", #smooth muscle
+"Mafb", #macrophage
+"Cybb", #monocyte
+"Flt3", #dc
+"Cpa3", #mast cell
+"Csf3r", #neutrophil
+"Ms4a1", #b cell
+"Klrd1", #nk cell
+"Il7r", #t cell
+"Prlr", #endometrium
+"Dcdc2a", #male epithelial
+"Erbb4" #female epithelial
+)
