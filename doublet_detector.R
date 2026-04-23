91afa28087989cd2f56e47d0fa54ad7c5b9b2d7c
Elisabeth Heuston <heustonef@nih.gov>
2026-04-23 09:59:24 -0400

    Revert "Minor bug fixes"
    This reverts commit 542380578f84e669769f2edbf079a56059c9a1b1.

--- doublet_detector.R
+++ doublet_detector.R
@@ -6,7 +6,6 @@ doublet_detector <- function(
 		logger,
 		sctransform = FALSE,
 		sourceable.functions = "RFunctions",
-		return_filtered = TRUE,
 		pANN.reuse = NULL,
 		doublet.var.thresh = 90, 
 		predicted.doubletRate = 0.05)
@@ -20,12 +19,6 @@ doublet_detector <- function(
 																			sctransformed = do.sctransform, 
 																			predicted.doubletRate = predicted.doubletRate,
 																			pANN.reuse = NULL)
-		if(return_filtered == TRUE){
-			seurat.object <- subset(
-				seurat.object, 
-				subset = DF.classifications == "Singlet"
-			)
-		}
 		return(seurat.object)
 	} else if(toupper(detector) == "SCRUBLET"){
 		msg <- "Running scrublet"
@@ -33,17 +26,11 @@ doublet_detector <- function(
 		log.msg(logger, msg = msg)
 		source(file.path(sourceable.functions, "runScrublet.R"))
 		seurat.object <- runScrublet(seurat.object)
-		if(return_filtered == TRUE){
-			seurat.object <- subset(
-				seurat.object, 
-				subset = is_doublet == FALSE
-			)
-		}
-			return(seurat.object)
-		} else {
-			msg <- "no doublet detector specified"
-			print(msg)
-			log.msg(logger, msg = msg)
-			
-		}
+		return(seurat.object)
+	} else {
+		msg <- "no doublet detector specified"
+		print(msg)
+		log.msg(logger, msg = msg)
+
+	}
 }
